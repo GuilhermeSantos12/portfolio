1687ada31621e0b68516bfd1907e2e026692a1cf
GuilhermeSantos12 <gs710424@gmail.com>
2021-04-29 20:27:59 -0300

    correção do arquivo env

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ru=ie**e^djqi#8(2aecp%l6=)#=ryk5r(ma)lv&mux18$@_4j'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
