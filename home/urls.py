db0666515b5cc6df640226c694c530e14795cbf2
GuilhermeSantos12 <gs710424@gmail.com>
2021-05-03 16:39:46 -0300

    Baixar button

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('',views.index, name='index'),
     path('projeto/',views.projeto, name='projeto'),
+
+
+
+
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import mimetypes
+
 
 def index(request):
     return render(request, 'home/index.html')
 
 def projeto(request):
     return render(request, 'projeto/projeto.html')
+
+
+
